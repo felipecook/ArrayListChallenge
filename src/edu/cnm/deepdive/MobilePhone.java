8c94cd6c2283a1937d5158c5b7366667813d2214
Felipe Cook <felipepcook@gmail.com>
2019-05-19 15:24:22 -0700

    Solved bug where you could update a contact with a contact already in the list.

--- src/edu/cnm/deepdive/MobilePhone.java
+++ src/edu/cnm/deepdive/MobilePhone.java
@@ -65,6 +65,10 @@ public class MobilePhone {
     if (foundPosition < 0) {
       System.out.println(oldContact.getName() + " was not found.");
       return false;
+    } else if (findContact(newContact.getName()) != -1) {
+      System.out.println("Contact with name: " + newContact.getName() +
+          " already exists, update was not successful.");
+      return false;
     }
 
     this.myContacts.set(foundPosition, newContact);
